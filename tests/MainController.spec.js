0d380dd4bd5ead33b7f39e364df0f018ab56c9c3
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-09-19 14:10:00 -0400

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,21 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+describe('MainController', function() {
+// this tells jasmine to use the app module
+// for every test for MainController
+  beforeEach(module('app'));
+
+  var $controller;
+// injecting controller service given by ngMock
+// in order to call a function with the controller we'd like
+// and also specify a custom $scope for the controller
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  // building test to see if name is set correctly in controller
+  it('should have Steve Jobs name', function() {
+    var $scope = {};
+ 
+    var controller = $controller('MainController', { $scope: $scope });
+    expect(controller.name).toEqual('Steve Jobs');
+  });
 });
